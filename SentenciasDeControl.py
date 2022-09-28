4ba8d60cab12866988ff996a9cb6962ff14f5c78
Juan David Espitia Aguillon <juandespitia14@gmail.com>
2022-09-27 20:12:28 -0500

    add sentecenes control class

---
+++ SentenciasDeControl.py
@@ -0,0 +1,53 @@
+# Sentencia de control IF
+condicion = True
+
+if condicion:
+    print("Condicion Verdadera")
+else:
+    print("Condicion Falsa")
+"""
+OJO:
+Cualquier valor difente de vacio es VERDADERO
+"""
+condicion = 10
+if condicion:
+    print("Condicion Verdadera")
+else:
+    print("Condicion Falsa")
+
+# Ejemplo 2
+condicion = ''
+if condicion:
+    print("Condicion Verdadera")
+else:
+    print("Condicion Falsa")
+
+# Aseguramos el valor Booleano
+condicion = 'hola'
+if condicion == True:
+    print("Condicion Verdadera")
+elif condicion == False:
+    print("Condicion Falsa")
+else:
+    print("Condicion no reconocida")
+
+# Validacion del Modo Debug
+# Aseguramos el valor Booleano
+condicion = 10
+if condicion == True:
+    print("Condicion Verdadera")
+elif condicion == False:
+    print("Condicion Falsa")
+else:
+    print("Condicion no reconocida")
+
+
+
+
+
+
+
+
+
+
+
