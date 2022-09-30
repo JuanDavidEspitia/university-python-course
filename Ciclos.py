1296de32b91b6dcdf4ce10a7f7aba4889a1a0f91
Juan David Espitia Aguillon <juandespitia14@gmail.com>
2022-09-29 20:28:41 -0500

    Loop for

--- Ciclos.py
+++ Ciclos.py
@@ -20,3 +20,38 @@ while contador > 0:
 else:
     print('Fin ciclo while')
 
+# Ciclo FOR
+
+cadena = "Hola"
+# Iteramos una cadena
+for letra in cadena:
+    print(letra)
+
+# Palabra BREAK en Python
+# la palabra Break se usa para romper el ciclo
+for letra in 'Holanda':
+    if letra == 'a':
+        print("El valor es: ", letra)
+        break
+    else:
+        print("Fin de la condicion")
+
+# Palabra CONTINUE
+# la funcion range imprime valores de 0 al valor que le ingresemos
+for i in range(6):
+    if i % 2 == 0:
+        print(f'Valor Par: {i}')
+
+# Usamos la palabra continue, desdes de esta palabra no ejecuta el codigo siguiente y comienza la siguiente iteracion
+for i in range(6):
+    if i % 2 != 0:
+        print(f'Valor Impar: {i}')
+        continue
+
+
+
+
+
+
+
+
