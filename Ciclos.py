de452dfe11a2e76b361d5964849e1c4eded3ecc1
Juan David Espitia Aguillon <juandespitia14@gmail.com>
2022-09-29 20:15:04 -0500

    Loop while

---
+++ Ciclos.py
@@ -0,0 +1,22 @@
+# Sentencia While
+# condicion = True
+#
+# while condicion:
+#     print('Ejecutando ciclo while')
+# else:
+#     print('Fin del ciclo while')
+contador = 0
+while contador < 3:
+    print(contador)
+    contador += 1 #contador = contador + 1
+else:
+    print('Fin ciclo while')
+
+print("While con contador inverso")
+contador = 5
+while contador > 0:
+    print(contador)
+    contador -= 1 #contador = contador + 1
+else:
+    print('Fin ciclo while')
+
