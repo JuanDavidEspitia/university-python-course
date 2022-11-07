01a09025b373a63ed113ed69530efad9e9ea2a11
JuanDavidEspitia <juandespitia14@gmail.com>
2022-11-06 20:26:28 -0500

    Polimorfismo

---
+++ polimorfismo/test_polimorfismo.py
@@ -0,0 +1,20 @@
+from Empleado import Empleado
+from Gerente import Gerente
+
+
+def imprimir_detalles(objeto):
+    # print(objeto)
+    print(type(objeto))
+    print(objeto.mostrar_detalles())
+    # Metodo isinstance nos permite preguntar si la instacia que queremos
+    # validar corresponde a una instancia de cierta clase
+    # si el objeto recibo pertenece a cierta instancia
+    if isinstance(objeto, Gerente):
+        print(objeto.departamento)
+
+
+empleado = Empleado('Juan', 5000)
+imprimir_detalles(empleado)
+
+gerente = Gerente('Karla', 6000, 'Sistemas')
+imprimir_detalles(gerente)
